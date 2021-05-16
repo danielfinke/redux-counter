53821e65ddeb616e111a50ec6a2400bfc1359a1c
Daniel Finke <danielfinke2011@gmail.com>
2021-05-16 09:02:59 -0700

    Add decrement functionality

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux';
 import './styles.scss';
 
 const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
 
 const initialState = {
   count: 0,
@@ -14,9 +15,10 @@ const initialState = {
 const reducer = (state = initialState, { type }) => {
   switch (type) {
     case INCREMENT:
+    case DECREMENT:
       return {
         ...state,
-        count: state.count + 1,
+        count: state.count + (type === INCREMENT ? 1 : -1),
       };
     default:
       return state;
@@ -24,19 +26,20 @@ const reducer = (state = initialState, { type }) => {
 };
 
 const incrementAction = { type: INCREMENT };
+const decrementAction = { type: DECREMENT };
 
 const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
-    const { count, increment } = this.props;
+    const { count, increment, decrement } = this.props;
 
     return (
       <main className="Counter">
         <p className="count">{count}</p>
         <section className="controls">
           <button onClick={increment}>Increment</button>
-          <button>Decrement</button>
+          <button onClick={decrement}>Decrement</button>
           <button>Reset</button>
         </section>
       </main>
@@ -47,6 +50,7 @@ class Counter extends Component {
 const mapStateToProps = ({ count }) => ({ count });
 const mapDispatchToProps = (dispatch) => ({
   increment: () => dispatch(incrementAction),
+  decrement: () => dispatch(decrementAction),
 });
 
 const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
