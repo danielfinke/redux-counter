46d0af6df82179ce9fdc5dbefc806cad745b40e0
Daniel Finke <danielfinke2011@gmail.com>
2021-05-16 09:01:18 -0700

    Create Redux store and add increment functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { connect, Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './styles.scss';
 
+const INCREMENT = 'INCREMENT';
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state = initialState, { type }) => {
+  switch (type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const incrementAction = { type: INCREMENT };
+
+const store = createStore(reducer);
+
 class Counter extends Component {
   render() {
+    const { count, increment } = this.props;
+
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
+          <button onClick={increment}>Increment</button>
           <button>Decrement</button>
           <button>Reset</button>
         </section>
@@ -18,4 +44,16 @@ class Counter extends Component {
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = ({ count }) => ({ count });
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(incrementAction),
+});
+
+const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <ConnectedCounter />
+  </Provider>,
+  document.getElementById('root'),
+);
